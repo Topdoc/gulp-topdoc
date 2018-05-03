b28c5326179eff79d667fa7da02bd7b1ef35f4dc
Garth Braithwaite <garthdb@gmail.com>
2018-05-02 23:57:33 -0700

    0.1.0 (#1)
    * Initial commit
    * initial release

---
+++ src/index.js
@@ -0,0 +1,40 @@
+import through from 'through2';
+import PluginError from 'plugin-error';
+import postcss from 'postcss';
+import topdoc from 'postcss-topdoc';
+
+// consts
+const PLUGIN_NAME = 'gulp-topdoc';
+
+export default function gulpTopdoc(opts) {
+  const stream = through.obj((file, enc, cb) => {
+    if (file.isStream()) {
+      return cb(new PluginError(PLUGIN_NAME, 'Streaming not supported'));
+    }
+    return postcss(topdoc(opts))
+      .process(file.contents, {
+        from: file.path
+      })
+      .then((result) => {
+        file.topdoc = result.topdoc;
+        stream.push(file);
+        return cb();
+      })
+      .catch(error => {
+        const errorOptions = {
+          fileName: file.path,
+          showStack: true
+        }
+        errorOptions.error = error
+        errorOptions.fileName = error.file || file.path
+        errorOptions.lineNumber = error.line
+        errorOptions.showProperties = false
+        errorOptions.showStack = false
+        error.message + '\n\n' + error.showSourceCode() + '\n'
+        setImmediate(function() {
+          cb(new PluginError(PLUGIN_NAME, error, errorOptions))
+        })
+      });
+  });
+  return stream;
+}
